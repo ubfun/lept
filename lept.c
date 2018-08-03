9d6b3fa87bac6198e442672a2133bfb9cd6b9824
xianhui <definezxh@163.com>
2018-08-03 19:47:01 +0800

    refactor lept_parse_literal

--- lept.c
+++ lept.c
@@ -21,13 +21,15 @@ static void lept_parse_whitespace(lept_context* c)
     c->json = p;
 }
 
-static int lept_parse_null(lept_context* c, lept_value* v)
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type)
 {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
+    size_t i;
+    EXPECT(c, literal[0]);
+    for (i = 0; literal[i + 1]; i++)
+        if (c->json[i] != literal[i + 1])
+            return LEPT_PARSE_INVALID_VALUE;
+    c->json += i;
+    v->type = type;
     return LEPT_PARSE_OK;
 }
 
@@ -35,7 +37,11 @@ static int lept_parse_value(lept_context* c, lept_value* v)
 {
     switch (*c->json) {
     case 'n':
-        return lept_parse_null(c, v);
+        return lept_parse_literal(c, v, "null", LEPT_NULL);
+    case 't':
+        return lept_parse_literal(c, v, "true", LEPT_TRUE);
+    case 'f':
+        return lept_parse_literal(c, v, "false", LEPT_FALSE);
     case '\0':
         return LEPT_PARSE_EXPECT_VALUE;
     default:
@@ -46,11 +52,17 @@ static int lept_parse_value(lept_context* c, lept_value* v)
 int lept_parse(lept_value* v, const char *json)
 {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0')
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v)
