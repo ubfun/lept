93dd7911d79dc29d8a009d8da734cfd7d62fa134
xianhui <definezxh@163.com>
2018-08-03 12:27:24 +0800

    parse null

---
+++ lept.h
@@ -0,0 +1,26 @@
+//
+// Author:  xianhui (definezxh@163.com)
+// Date:    2018/08/03 09:43:57
+//
+
+#ifndef LEPT_H
+#define LEPT_H
+
+typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+
+typedef struct {
+    lept_type type;
+} lept_value;
+
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+
+int lept_parse(lept_value* v, const char* json);
+
+lept_type lept_get_type(const lept_value* v);
+
+#endif // LEPT_H
---
+++ test.c
@@ -0,0 +1,45 @@
+//
+// Author:  xianhui (definezxh@163.com)
+// Date:    2018/08/03 11:07:32
+//
+
+#include "lept.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
+	do { \
+		test_count++; \
+		if (equality) \
+			test_pass++; \
+		else { \
+			fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual); \
+			main_ret = 1; \
+		} \
+	} while (0)
+
+#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+
+static void test_parse_null()
+{
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+void test()
+{
+    test_parse_null();
+}
+
+int main()
+{
+    test();
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+}
